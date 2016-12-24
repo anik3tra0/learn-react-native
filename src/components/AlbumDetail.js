f25b7cf55a5a08ece47cc52d8700cc08fbaf9976
anik3tra0 <anik3tra0@gmail.com>
2016-12-24 14:48:58 +0530

    Functional Props

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   const { title, artist, thumbnail_image, image } = album;
-  const { headerContentStyle, thumbnailStyle, thumbnailContainerStyle, headerTextStyle, albumArtworkStyle } = styles;
+  const { headerContentStyle,
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    albumArtworkStyle
+  } = styles;
 
   return (
     <Card>
+
       <CardSection>
         <View style={thumbnailContainerStyle}>
           <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
@@ -18,9 +25,15 @@ const AlbumDetail = ({ album }) => {
           <Text>{artist}</Text>
         </View>
       </CardSection>
+
       <CardSection>
         <Image style={albumArtworkStyle} source={{ uri: image }} />
       </CardSection>
+
+      <CardSection>
+        <Button onPress={() => console.log(title)} />
+      </CardSection>
+
     </Card>
   );
 };
